b48d6b2f6e15dc4d9a25907065d0dc6e18d6b843
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2020-04-03 09:50:52 +0000

    Add modifier to roll command

--- src/commands/rollCommand.js
+++ src/commands/rollCommand.js
@@ -4,7 +4,12 @@ import db from '../db'
 const formatMessage = createMessageFormatter()
 
 const processRoll = (message, args) => {
-  const [user, attribute, skill] = args
+  if (message.mentions.users.size !== 1) {
+    message.reply('You must tag one player :angry:')
+    return
+  }
+
+  const [user, attribute, skill, modifier = ''] = args
 
   const discordId = user.slice(3, user.length - 1)
 
@@ -19,7 +24,8 @@ const processRoll = (message, args) => {
           attrValue,
           skill,
           skillValue || 0,
-          rollD10()
+          rollD10(),
+          modifier
         )
       )
     })
@@ -30,7 +36,7 @@ function createMessageFormatter (lineWidth = 40) {
     left + ':'.padEnd(lineWidth - (left.length + right.length + 1)) + right + '\n'
 
   /**
-   * Formats the roll message.
+   * Formats the roll message and calculates total.
    * @param {string} charName - name of the character
    * @param {string} attribute - name of the attribute
    * @param {number} attrValue - attribute's value
@@ -39,13 +45,19 @@ function createMessageFormatter (lineWidth = 40) {
    * @param {number} diceRoll - value of the dice roll
    * @returns {string} formatted message
    */
-  const formatMessage = (charName, attribute, attrValue, skill, skillValue, diceRoll) => {
-    const total = attrValue + skillValue + diceRoll
+  const formatMessage = (charName, attribute, attrValue, skill, skillValue, diceRoll, modifier = '') => {
+    const intModifier = Number.parseInt(modifier)
+
+    const total = Number.isNaN(intModifier)
+      ? attrValue + skillValue + diceRoll
+      : attrValue + skillValue + diceRoll + intModifier
+
     return '```\n' +
       `${charName} rolls:\n` +
       formatLine(attribute, attrValue.toString()) +
       formatLine(skill, skillValue.toString()) +
       formatLine('d10', diceRoll.toString()) +
+      (Number.isNaN(intModifier) ? '' : formatLine('Modifier', modifier)) +
       ''.padEnd(lineWidth, '-') + '\n' +
       formatLine('Total', total.toString()) +
       '```\n'
@@ -54,21 +66,28 @@ function createMessageFormatter (lineWidth = 40) {
   return formatMessage
 }
 
+/**
+ * Fetches the db for the attr and skill values of the character
+ * assigned to the discord user (by means of a 6-story select)
+ * @param {string} discordId - ID of the discord user
+ * @param {string} attribute - name of the attribute
+ * @param {string} skill - name of the skill
+ */
 function fetchCharacterStats (discordId, attribute, skill) {
   return db.query(`
-  SELECT
-    c.name AS 'charName',
-    chatt.attr_value AS 'attrValue',
-    chski.skill_value as 'skillValue'
-  FROM characters c
-    LEFT JOIN char_attrs chatt ON chatt.char_id = c.id
-    LEFT JOIN (
-      SELECT * FROM char_skills WHERE skill_id=(
-        SELECT id FROM skills WHERE name='${skill}'
-      )
-    ) chski ON chski.char_id = c.id
-  WHERE c.user_id = (SELECT id FROM users WHERE discord_id="${discordId}") 
-    AND chatt.attribute_id = (SELECT id FROM attributes WHERE name="${attribute}");
+    SELECT
+      c.name AS 'charName',
+      chatt.attr_value AS 'attrValue',
+      chski.skill_value as 'skillValue'
+    FROM characters c
+      LEFT JOIN char_attrs chatt ON chatt.char_id = c.id
+      LEFT JOIN (
+        SELECT * FROM char_skills WHERE skill_id=(
+          SELECT id FROM skills WHERE name='${skill}'
+        )
+      ) chski ON chski.char_id = c.id
+    WHERE c.user_id = (SELECT id FROM users WHERE discord_id="${discordId}") 
+      AND chatt.attribute_id = (SELECT id FROM attributes WHERE name="${attribute}");
   `)
 }
 
